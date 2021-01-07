be10fd2895f068991d3a1b05a7dae677ff09f09a
TheEvilSkeleton <theevilskeleton@riseup.net>
2021-01-07 14:16:52 -0500

    Update Zypak to c63a87a49898299bb803408eecffbcc1e44a65e6

--- edge.sh
+++ edge.sh
@@ -42,5 +42,6 @@ fi
 export XCURSOR_PATH=$(echo "$XDG_DATA_DIRS" | sed 's,\(:\|$\),/icons\1,g')
 export CHROME_WRAPPER=$(readlink -f "$0")
 export TMPDIR="$XDG_RUNTIME_DIR/app/$FLATPAK_ID"
+export ZYPAK_SANDBOX_FILENAME=msedge-sandbox
 
 exec zypak-wrapper.sh /app/extra/msedge "$@"
